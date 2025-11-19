1a118bd60f591378d60168d60e58f70fd779838f
yuyetingxiao <2939708850@qq.com>
2025-11-19 21:02:46 +0800

    Foundation class:AssignmentTwo class

---
+++ src/AssignmentTwo.java
@@ -0,0 +1,16 @@
+public class AssignmentTwo {
+        public static void main(String[] args) {
+        }
+
+        public void partThree() {}
+
+        public void partFourA() {}
+
+        public void partFourB() {}
+
+        public void partSix() {}
+
+        public void partSeven() {}
+
+        public void partFive() {}
+}
