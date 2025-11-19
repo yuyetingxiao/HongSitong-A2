c587eb4b7f3924e33757fb3fce0cdfbff91032b0
yuyetingxiao <2939708850@qq.com>
2025-11-19 21:58:41 +0800

    Abstract class and RideInterface interface

---
+++ src/RideInterface.java
@@ -0,0 +1,13 @@
+public interface RideInterface {
+
+        void addVisitorToQueue(Visitor visitor);
+        void removeVisitorFromQueue();
+        void printQueue();
+
+        void addVisitorToHistory(Visitor visitor);
+        boolean checkVisitorFromHistory(Visitor visitor);
+        int numberOfVisitors();
+        void printRideHistory();
+
+        void runOneCycle();
+}
