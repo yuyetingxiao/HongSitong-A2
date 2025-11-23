c1305f2c61d6d18e9eb673bc9fa37a26577d3712
yuyetingxiao <2939708850@qq.com>
2025-11-23 22:54:26 +0800

    Waiting queue function: Queue implementation

--- src/Ride.java
+++ src/Ride.java
@@ -1,8 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Ride implements RideInterface {
 
         private String rideName;
         private String rideType;
         private Employee operator;
+        private Queue<Visitor> waitingQueue = new LinkedList<>();
 
         public Ride() {
         }
@@ -39,17 +43,36 @@ public class Ride implements RideInterface {
 
     @Override
     public void addVisitorToQueue(Visitor visitor) {
-
+            if (visitor != null) {
+                waitingQueue.offer(visitor);
+                System.out.println("Successfully added a visitor to [" + rideName + "] the waiting queue：" + visitor.getName());
+            } else {
+                System.out.println("Error: Invalid tourist object, cannot be added to the queue!");
+            }
     }
 
     @Override
     public void removeVisitorFromQueue() {
-
+            if (waitingQueue.isEmpty()) {
+                System.out.println("Corrected translation: The waiting queue for [" + rideName + "] is empty. It is not possible to remove the visitor!");
+            } else {
+                Visitor removedVisitor = waitingQueue.poll();
+                System.out.println("Successfully removed a visitor from the waiting queue at [" + rideName + "]:" + removedVisitor.getName());
+            }
     }
 
     @Override
     public void printQueue() {
-
+            if (waitingQueue.isEmpty()) {
+                System.out.println("[" + rideName + "]'s waiting queue is empty!");
+                return;
+            }
+            System.out.println("[" + rideName + "]'s waiting queue（A total of " + waitingQueue.size() + " people）");
+            int index = 1;
+            for (Visitor v : waitingQueue) {
+                System.out.println(index + ". " + v);
+                index++;
+            }
     }
 
     @Override
